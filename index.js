8f44e277da0d37cc1d09a26138a159ed556c1fe3
hamza <mahmoudhamza9440@gmail.com>
2023-03-12 21:18:46 +0200

    v2

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const btns = document.querySelectorAll('.btn-add');
 const tasksLists = document.querySelectorAll('.tasks-list');
-const draggables = document.querySelectorAll('.task-wrapper');
 const droppables = document.querySelectorAll('.tasks-list');
 
 const init = () => {
+  // restore all saved content from localStorage
+  droppables.forEach((track) => {
+    const key = track.id;
+    let values = window.localStorage.getItem(key);
+    values = values ? values.split(',') : [];
+    print(track, values);
+
+    // add listener for dragover event
+    track.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      const bottomTask = insertAbove(track, e.clientY);
+      const curTask = document.querySelector('.dragging');
+      if (!bottomTask) {
+        track.appendChild(curTask);
+      } else {
+        track.insertBefore(curTask, bottomTask);
+      }
+    });
+  });
+
+  // liten to clicks on Add btns to create new tasks
   btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       createNewTask(e.target.previousElementSibling);
@@ -13,31 +33,17 @@ const init = () => {
   tasksLists.forEach((list) => {
     // listenfocus-out on cards
     list.addEventListener('focusout', (e) => {
+      // check if the element contains 'focus' class before we store it's value
+      if (e.target.classList.contains('focus')) {
+        store();
+      }
       e.target.setAttribute('readonly', 'true');
       e.target.classList.remove('focus');
     });
+
     // listen to click on edit or delete btns
     list.addEventListener('click', (e) => {
       handleClickTask(e.target);
-    });
-  });
-
-  draggables.forEach((task) => {
-    //listening for start and end of dragging
-    addStartEndDragListener(task);
-  });
-
-  droppables.forEach((track) => {
-    track.addEventListener('dragover', (e) => {
-      e.preventDefault();
-      const bottomTask = insertAbove(track, e.clientY);
-      const curTask = document.querySelector('.dragging');
-
-      if (!bottomTask) {
-        track.appendChild(curTask);
-      } else {
-        track.insertBefore(curTask, bottomTask);
-      }
     });
   });
 
@@ -51,17 +57,19 @@ const init = () => {
 function createNewTask(parent) {
   const html = `
     <div class="task-wrapper" draggable="true">
-      <textarea name="task" placeholder="new task" class="task"></textarea>
+      <input name="task" placeholder="new task" class="task"></input>
       <div class="task-icons">
         <ion-icon name="create"></ion-icon>
         <ion-icon name="trash"></ion-icon>
       </div>
     </div>`;
   parent.insertAdjacentHTML('beforeend', html);
+
   const newCreatedTask = parent.querySelector('.task-wrapper:last-child .task');
   newCreatedTask.classList.add('focus');
   newCreatedTask.focus();
 
+  // select the last created task
   const task = parent.querySelector('.task-wrapper:last-child');
   addStartEndDragListener(task);
 }
@@ -76,6 +84,7 @@ function handleClickTask(target) {
   } else if (targetBtn === 'trash') {
     const cardContainer = target.parentElement.parentElement;
     cardContainer.remove();
+    store();
   }
 }
 
@@ -86,6 +95,7 @@ function addStartEndDragListener(task) {
   });
   task.addEventListener('dragend', (e) => {
     task.classList.remove('dragging');
+    store();
   });
 }
 
@@ -107,4 +117,32 @@ function insertAbove(track, mouseY) {
   return closestTask;
 }
 
+function store() {
+  droppables.forEach((container) => {
+    const key = container.id;
+    const values = [];
+    for (const child of container.children) {
+      values.push(child.firstElementChild.value);
+    }
+    window.localStorage.setItem(key, values.toString());
+  });
+}
+
+function print(track, values) {
+  values.forEach((element) => {
+    const html = `
+    <div class="task-wrapper" draggable="true">
+      <input name="task" placeholder="new task" value="${element}" class="task"></input>
+      <div class="task-icons">
+        <ion-icon name="create"></ion-icon>
+        <ion-icon name="trash"></ion-icon>
+      </div>
+    </div>`;
+    track.insertAdjacentHTML('beforeend', html);
+
+    // add drag listener for tasks printend on the screen
+    const task = track.querySelector('.task-wrapper:last-child');
+    addStartEndDragListener(task);
+  });
+}
 init();
